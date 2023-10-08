09e36bea542bb65d989be2d9011496b331deb50d
Elidemuk7 <elijahmukoro7@gmail.com>
2023-10-08 04:17:45 -0700

    tsk8

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -2,7 +2,7 @@
 def uppercase(str):
     """converting to uppercase"""
     for i in str:
-        if orc(i) >= 97 and orc(i) <= 122:
+        if ord(i) >= 97 and ord(i) <= 122:
             char = chr(orc(i) - 32)
         print("{}".format(char), end= "")
     print("")
