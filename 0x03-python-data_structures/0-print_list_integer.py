802e27d55e550753e1fa3503a2f71ec08c03333c
Elidemuk7 <elijahmukoro7@gmail.com>
2023-10-10 14:00:08 -0700

    print elements of list

---
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+"""
+A function that prints the elements of a list
+"""
+
+def print_list_integer(my_list=[]):
+    for i in range(len(my_list)):
+        if i >= 0:
+            print("{}".format(my_list[i]))
