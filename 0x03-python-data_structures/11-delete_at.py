35fcf09834b6b99b87850637baa728d23eebd694
Elidemuk7 <elijahmukoro7@gmail.com>
2023-10-15 04:02:24 -0700

    list del

--- 0x03-python-data_structures/11-delete_at.py
+++ 0x03-python-data_structures/11-delete_at.py
@@ -2,8 +2,6 @@
 
 def delete_at(my_list=[], idx=0):
     """deletes at a specific index"""
-
-    for idx in len(my_list):
-        if idx >= 0:
-            del my_list[idx]
-        return (my_list)
+    if idx >= 0 and idx < len(mylist):
+        del my_list[idx]
+    return (my_list)
